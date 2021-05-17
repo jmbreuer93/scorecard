599cf80b757782713bd0301177d63074d0bf341e
Josh Breuer <63369933+jmbreuer93@users.noreply.github.com>
2021-05-16 20:34:41 -0400

    add winning percentage to teams model

--- models/team.js
+++ models/team.js
@@ -1,39 +1,50 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Float = require('mongoose-float').loadType(mongoose, 3);
 const Player = require('./player');
 const Game = require('./game');
 
 const TeamSchema = new Schema({
-	name    : {
+	name              : {
 		type     : String,
 		required : true
 	},
-	manager : {
+	manager           : {
 		type     : String,
 		required : true
 	},
-	players : [
+	players           : [
 		{
 			type : Schema.Types.ObjectId,
 			ref  : 'Player'
 		}
 	],
-	wins    : {
+	wins              : {
 		type     : Number,
 		required : true,
 		default  : 0
 	},
-	losses  : {
+	losses            : {
 		type     : Number,
 		required : true,
 		default  : 0
 	},
-	games   : [
+	games             : [
 		{
 			type : Schema.Types.ObjectId,
 			ref  : 'Game'
 		}
-	]
+	],
+	winningPercentage : {
+		type     : Float,
+		default  : function () {
+			if (this.games) {
+				return this.wins / this.games.length;
+			}
+			return 0;
+		},
+		required : true
+	}
 });
 
 module.exports = module.model('Team', TeamSchema);
