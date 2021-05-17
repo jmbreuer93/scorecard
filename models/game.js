d5140736bdbd03e0065a38b1205a56a908a81d5a
Josh Breuer <63369933+jmbreuer93@users.noreply.github.com>
2021-05-16 20:25:22 -0400

    create game model

---
+++ models/game.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Player = require('./player');
+const Team = require('./team');
+
+const GameSchema = new Schema({
+	homeTeam : {
+		type : Schema.Types.ObjectId,
+		ref  : 'Team'
+	},
+	awayTeam : {
+		type : Schema.Types.ObjectId,
+		ref  : 'Team'
+	},
+	date     : {
+		type     : Date,
+		required : true
+	},
+	time     : {
+		type     : Date,
+		required : true
+	}
+});
+
+module.exports = module.model('Game', GameSchema);
